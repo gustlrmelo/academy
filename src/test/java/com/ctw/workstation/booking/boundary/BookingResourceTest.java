2ac91fd0ddca99f2d1b96b5ba4409420931cee8a
Gustavo <ctw03677@criticaltechworks.com>
2024-08-02 12:14:01 +0100

    tests commented

--- src/test/java/com/ctw/workstation/booking/boundary/BookingResourceTest.java
+++ src/test/java/com/ctw/workstation/booking/boundary/BookingResourceTest.java
@@ -1,31 +1,32 @@
-package com.ctw.workstation.booking.boundary;
-
-import com.ctw.workstation.booking.entity.BookingRepository;
-import com.ctw.workstation.config.DatabaseTestResource;
-import io.quarkus.test.InjectMock;
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.junit.QuarkusTest;
-import jakarta.inject.Inject;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-
-@QuarkusTest
-@QuarkusTestResource(DatabaseTestResource.class)
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class BookingResourceTest {
-
-    @Inject
-    BookingResource bookingResource;
-
-   @InjectMock
-   BookingRepository bookingRepository;
-
-
-    @Test
-    void doIt(){
-
-
-    }
-
-}
+//
+//package com.ctw.workstation.booking.boundary;
+//
+//import com.ctw.workstation.booking.entity.BookingRepository;
+//import com.ctw.workstation.config.DatabaseTestResource;
+//import io.quarkus.test.InjectMock;
+//import io.quarkus.test.common.QuarkusTestResource;
+//import io.quarkus.test.junit.QuarkusTest;
+//import jakarta.inject.Inject;
+//import org.junit.jupiter.api.MethodOrderer;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.TestMethodOrder;
+//
+//@QuarkusTest
+//@QuarkusTestResource(DatabaseTestResource.class)
+////@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//class BookingResourceTest {
+//
+//    @Inject
+//    BookingResource bookingResource;
+//
+//   @InjectMock
+//   BookingRepository bookingRepository;
+//
+//
+//    @Test
+//    void doIt(){
+//
+//
+//    }
+//
+//}
